d1a97f2c6976077d00a8594fabcce56218d7632b
Burkhard Hampl <bhampl@student.tgm.ac.at>
2015-01-07 17:37:51 +0100

    Added CLI and Main

--- src/main/java/at/hackenbergerhampl/picalculator/server/CLIParser.java
+++ src/main/java/at/hackenbergerhampl/picalculator/server/CLIParser.java
@@ -1,21 +1,60 @@
 package at.hackenbergerhampl.picalculator.server;
 
+import java.io.ByteArrayOutputStream;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import at.hackenbergerhampl.picalculator.error.InvalidOptionException;
+
+/**
+ * A Parser to parse options and arguments from the CLI for {@link PIRemoteServer}
+ * 
+ * @author Burkhard Hampl
+ * @version 1.0
+ */
 public class CLIParser {
 
+	
+	
+
+	@Option(name = "-p", usage = "the port on which the PIRemoteServer should bind on or the port on which the PIBalancer listen on (default 1099)")
+	private int port = 1099;
+
+	@Option(name = "-h", usage = "the ip or domain where the PIBalancer is")
 	private String host;
 
-	private int port;
+	
+	/**
+	 * This method starts the parsing of the options and arguments
+	 *
+	 * @param args
+	 *            the arguments from the main method
+	 * @param usage
+	 *            a usage String of how to start the program
+	 * @throws InvalidOptionException
+	 *             if any thing goes wrong while parsing (ex: a required option
+	 *             is missing)
+	 */
+	public void parse(String[] args, String usage) throws InvalidOptionException {
+		CmdLineParser parser = new CmdLineParser(this);
 
-	public void parse(String[] args) {
-
-	}
-
-	public String getHost() {
-		return null;
+		try {
+			parser.parseArgument(args);
+		} catch (CmdLineException ex) {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			parser.printUsage(out);
+			throw new InvalidOptionException(ex.getMessage() + '\n' + usage + '\n' + out.toString());
+		}
 	}
 
 	public int getPort() {
-		return 0;
+		return this.port;
+	}
+
+	public String getHost() {
+		return this.host;
 	}
 
 }
--- src/main/java/at/hackenbergerhampl/picalculator/server/Main.java
+++ src/main/java/at/hackenbergerhampl/picalculator/server/Main.java
@@ -1,9 +1,31 @@
 package at.hackenbergerhampl.picalculator.server;
 
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+
+import at.hackenbergerhampl.picalculator.error.InvalidOptionException;
+
 public class Main {
-
-	public static void main() {
-
+	public static void main(String[] args) {
+		CLIParser p = new CLIParser();
+		try {
+			p.parse(args, "java -jar PIRemoteServer.jar [options]");
+		} catch (InvalidOptionException ex) {
+			System.out.println(ex.getMessage());
+			System.exit(1);
+			;
+		}
+		try {
+			if (p.getHost() == null) {
+			new PIRemoteServer(p.getPort());
+			} else {
+				new PIRemoteServer(p.getHost(), p.getPort());
+			}
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (AlreadyBoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
