ea14c5acc0f91a5d7b18fbedba7dab373dbccf73
Burkhard Hampl <bhampl@student.tgm.ac.at>
2015-01-08 16:27:05 +0100

    Added ShutdownHook to PIRemoteServer and PIBalancer

--- src/main/java/at/hackenbergerhampl/picalculator/balancer/PIBalancer.java
+++ src/main/java/at/hackenbergerhampl/picalculator/balancer/PIBalancer.java
@@ -15,8 +15,8 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
- * Represents a {@link PIBalancer} which balances request for pi calculation and direct them to a
- * calculation server
+ * Represents a {@link PIBalancer} which balances request for pi calculation and
+ * direct them to a calculation server
  * 
  * @author Hackenberger Christoph
  * @version 1.0
@@ -29,18 +29,28 @@ public class PIBalancer extends UnicastRemoteObject implements RemoteCalculator,
 	private static final long serialVersionUID = 1973999418059633612L;
 	private ConcurrentLinkedQueue<RemoteCalculator> servers = new ConcurrentLinkedQueue<>();
 	private Registry reg;
-	
+
 	/**
 	 * Creates a new instance of a {@link PIBalancer}
 	 * 
-	 * @param port the port the registry should bind on
-	 * @throws RemoteException  if the registry could not be exported
-	 * @throws AlreadyBoundException if there is another {@link PIBalancer} or {@link PIRemoteServer} is already bound
+	 * @param port
+	 *            the port the registry should bind on
+	 * @throws RemoteException
+	 *             if the registry could not be exported
+	 * @throws AlreadyBoundException
+	 *             if there is another {@link PIBalancer} or
+	 *             {@link PIRemoteServer} is already bound
 	 */
 	public PIBalancer(int port) throws RemoteException, AlreadyBoundException {
 		super();
 		reg = LocateRegistry.createRegistry(port);
 		reg.bind("picalc", this);
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+			@Override
+			public void run() {
+				close();
+			}
+		}));
 		System.out.println("Balancer successfully started!");
 		System.out.println("Registy successfully bounded!");
 	}
@@ -48,29 +58,29 @@ public class PIBalancer extends UnicastRemoteObject implements RemoteCalculator,
 	/**
 	 * Delegates the pi calculation to one of his calculation servers
 	 * 
-	 * @param digits digits of pi
+	 * @param digits
+	 *            digits of pi
 	 * @return pi
 	 */
 	public BigDecimal pi(int digits) throws RemoteException {
 		System.out.println("PI request with " + digits + " digits received. Directing to server!");
-		while(true) {
+		while (true) {
 			RemoteCalculator rc = this.getNextRemoteCalculator();
-			if(rc != null) {
+			if (rc != null) {
 				try {
 					return rc.pi(digits);
-				}catch (RemoteException ex) {
+				} catch (RemoteException ex) {
 					servers.remove(rc);
 				}
-			}else
+			} else
 				return null;
 		}
 	}
 
 	public void addRemoteCalculator(RemoteCalculator server) throws RemoteException {
 		servers.add(server);
-		
-	}
 
+	}
 
 	public boolean removeRemoteCalculator(RemoteCalculator server) throws RemoteException {
 		return servers.remove(server);
@@ -78,7 +88,7 @@ public class PIBalancer extends UnicastRemoteObject implements RemoteCalculator,
 
 	public RemoteCalculator getNextRemoteCalculator() throws RemoteException {
 		RemoteCalculator rc = servers.poll();
-		if(rc != null)
+		if (rc != null)
 			servers.add(rc);
 		return rc;
 	}
